03804b04dd4637fa9ec519c49d32415c75c0ef12
Her Fri <herbert.frichert@outlook.de>
2023-10-12 23:16:03 +0200

    edit Heroku URL in ALLOWED_HOSTS

--- RetroReview/settings.py
+++ RetroReview/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['retroreview-herfri.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['retroreview-herfri-6b3894860994.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
