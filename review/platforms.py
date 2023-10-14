ca216a42e70c7d0f3842b37f0360554a9eef8000
Her Fri <herbert.frichert@outlook.de>
2023-10-14 16:53:49 +0200

    add platforms.py, migrate updated models

---
+++ review/platforms.py
@@ -0,0 +1,13 @@
+PLATFORMS = (
+    ("Dreamcast", "Dreamcast"),
+    ("Gameboy", "Gameboy"),
+    ("Gameboy Color", "Gameboy Color"),
+    ("NES", "NES"),
+    ("N64", "N64"),
+    ("PC", "PC"),
+    ("PlayStation", "PlayStation"),
+    ("PlayStation 2", "PlayStation 2"),
+    ("Sega Genesis", "Sega Genesis"),
+    ("SNES", "SNES"),
+    ("XBOX", "XBOX"),
+    )
