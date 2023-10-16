1c12eb3d64c5e2aa7e744a41a71df7d256d1b64c
Her Fri <herbert.frichert@outlook.de>
2023-10-17 00:24:53 +0200

    add approve_comments action

--- review/admin.py
+++ review/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Game, Review
+from .models import Game, Review, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -12,6 +12,15 @@ class ReviewAdmin (SummernoteModelAdmin):
     list_filter = ('status', 'created_on')   
     summernote_fields = ('content')
 
-
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'body', 'review', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('username', 'email', 'body')
+    actions = ['approve_comments']                      #
+                                                        #   
+    def approve_comments(self, request, queryset):      # needed in project?
+        queryset.update(approved=True)                  #
 # Register your models here.
 admin.site.register(Game)
+
