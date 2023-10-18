1f2e62613c586d92c0258b10083a80d119e92b5b
Her Fri <herbert.frichert@outlook.de>
2023-10-18 23:55:10 +0200

    add reviews.html, add urlpatterns, add ReviewDetail view

--- review/urls.py
+++ review/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.GameList.as_view(), name='home')
+    path('', views.GameList.as_view(), name='home'),
+#    path('<slug:slug>/', views.ReviewDetail.as_view(), name='review_detail'), #Slug converter!
+    path('<slug:slug>/', views.GameReviews.as_view(), name='reviews'), # TO BE EDITED!
 ]
--- review/views.py
+++ review/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Game, Review, Comment
 
 # Create your views here.
@@ -9,3 +9,32 @@ class GameList(generic.ListView):
     queryset = Game.objects.all()   # .order_by('-rating')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class GameReviews(generic.ListView):    # Site, where all Reviews are listed
+    model = Review
+    #slug = Game.slug                      # --> TO BE EDITED!
+    queryset = Review.objects.all()        #(slug=slug)      # filter() #all() # Review.objects.filter(slug=game.title)
+    template_name = 'reviews.html'       #for every game there is a different slug (URL)
+    paginate_by = 6
+
+
+class ReviewDetail(View):
+    
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Review.objects.all()     # in walktrough: objects.filter(status=1)
+        review = get_object_or_404(queryset, slug=slug)
+        comments = review.comments.all().order_by('created_on') # in walktrough: comments.filter(approved=True).order_by
+        liked = False
+        if review.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "review_detail.html",
+            {   
+                "review": review,
+                "comments": comments,
+                "liked": liked
+            },
+        )
