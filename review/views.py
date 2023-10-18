cf36a59350eaa31f4d108663db137d2809917be5
Her Fri <herbert.frichert@outlook.de>
2023-10-18 15:50:11 +0200

    add view for Game class, add template files, add css folder and file

--- review/views.py
+++ review/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Game, Review, Comment
 
 # Create your views here.
+
+class GameList(generic.ListView):
+    model = Game
+    queryset = Game.objects.all().order_by('-rating')
+    template_name = 'index.html'
+    paginate_by = 6
