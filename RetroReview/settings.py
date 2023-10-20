1aa479611e0fd1470cc15241c55310454b68d70f
Her Fri <herbert.frichert@outlook.de>
2023-10-20 23:26:49 +0200

    set DEBUG = False in settings.py

--- RetroReview/settings.py
+++ RetroReview/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['retroreview-herfri-6b3894860994.herokuapp.com', 'localhost', '127.0.0.1', '8000-herfri-retroreview-h1rwum0tx5f.ws-eu105.gitpod.io']
 
